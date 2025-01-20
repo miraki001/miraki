0dade05045dd04ec092201603dc0df07f8082db7
miraki <derquicia@mariogoldsteinsa.com.ar>
2025-01-20 15:51:10 -0300

    Update fuentes.py

--- pages/fuentes.py
+++ pages/fuentes.py
@@ -6,7 +6,7 @@ from streamlit_navigation_bar import st_navbar
 
 
 
-page = st_navbar(["Home", "Page 1", "Page 2"], selected="Page 2")
+#page = st_navbar(["Home", "Page 1", "Page 2"], selected="Page 2")
 
 
 def show_fuentes():
