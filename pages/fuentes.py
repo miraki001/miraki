e9b80234b096df0584dce1d7021ea6f561492e97
miraki <derquicia@mariogoldsteinsa.com.ar>
2025-01-20 13:32:06 -0300

    Update fuentes.py

--- pages/fuentes.py
+++ pages/fuentes.py
@@ -10,7 +10,7 @@ def show_fuentes():
   vdetalle = ''
   vlink = ''
   vimagen = ''
-  spage = st_navbar(["Home", "Documentation", "Examples", "Community", "About"])
+  spage = st_navbar(["Home", "Documentation", "Examples", "Community", "About"], selected="Page 1")
   st.write(spage)
   
 
