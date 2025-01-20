83ae5a59922e47db97160f2d76d0eb47418c52f7
miraki <derquicia@mariogoldsteinsa.com.ar>
2025-01-20 16:12:48 -0300

    Update fuentes.py

--- pages/fuentes.py
+++ pages/fuentes.py
@@ -11,5 +11,25 @@ def show_fuentes():
   vdetalle = ''
   vlink = ''
   vimagen = ''
+  col1, col2, col3,col4,col5,col6,col7 = st.columns(7)
+  if col1.button("Home" ,  type='primary'):
+    st.switch_page("streamlit_app.py")
+  if col2.button("Insertar"):
+    st.session_state['vTipo'] = 'Ingresar'
+    st.switch_page("./pages/editar_fuentes.py")
+  if col3.button("Editar"):   
+    st.write(st.session_state.vcnt)
+    st.write(st.session_state.cnt)
+    st.write(cnt)
+    st.session_state['vTipo'] = 'Editar'
+    st.switch_page("./pages/editar_fuentes.py")
+  if col4.button("Borrar", ):
+    st.switch_page("./pages/borrarfuente.py")   
+  if col5.button("Verificar"):
+    st.switch_page("./pages/verifpagbs.py")
+  if col6.button("Ejecutar"):
+    st.switch_page("./pages/scraptodo.py")
+  if col7.button("Duplicar"):
+    st.switch_page("./pages/duplicarfuente.py")
 
 
