f4bcd107b71c0f9802c89bee46b0d62a90b9315f
miraki <derquicia@mariogoldsteinsa.com.ar>
2025-01-20 16:03:19 -0300

    Update fuentes.py

--- pages/fuentes.py
+++ pages/fuentes.py
@@ -6,7 +6,7 @@ from streamlit_navigation_bar import st_navbar
 
 
 
-#page = st_navbar(["Home", "Page 1", "Page 2"], selected="Page 2")
+
 
 
 def show_fuentes():
@@ -15,42 +15,5 @@ def show_fuentes():
   vdetalle = ''
   vlink = ''
   vimagen = ''
-  pages = ["Home", "Ingresar", "Editar", "Borrar", "Duplicar", "Parametros", "GitHub"]
-  parent_dir = os.path.dirname(os.path.abspath(__file__))
-  styles = {
-      "nav": {
-          "background-color": "#604283",
-          "justify-content": "left",
-      },
-      "img": {
-          "padding-right": "14px",
-      },
-      "span": {
-          "color": "white",
-          "padding": "44px",
-      },
-      "active": {
-          "background-color": "white",
-          "color": "var(--text-color)",
-          "font-weight": "normal",
-          "padding": "14px",
-      }
-  }
-  options = {
-      "show_menu": False,
-      "show_sidebar": False,
-  }
-
-  page = st_navbar(
-      pages,
-      styles=styles,
-      options=options,
-  )
-  
-  st.write(page)
-
-  if page == "Home":
-    st.switch_page("miraki.py")
-
 
 
