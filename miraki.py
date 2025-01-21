c03db667be229c8f360b36ca21c03ad375ae4b4d
miraki <derquicia@mariogoldsteinsa.com.ar>
2025-01-21 19:20:00 -0300

    Update miraki.py

--- miraki.py
+++ miraki.py
@@ -32,7 +32,7 @@ selected = option_menu(None, ["Home", 'Novedades','Fuentes', 'Informes','Paramet
 selected1 = option_menu(None, ["Home", 'Ingresar','Editar', 'Informes','Parametros','Github' ], 
         icons=['house', 'gear' ,'gear'] , menu_icon="cast",orientation="horizontal", default_index=0,
         styles={
-        "container": {"padding": "0!important", "background-color": "#604283"},
+        "container": {"padding": "10!important", "background-color": "#604283"},
         "icon": {"color": "orange", "font-size": "14px"}, 
         "nav-link": {"font-size": "14px", "text-align": "left", "margin":"0px", "--hover-color": "#eee"},
         "nav-link-selected": {"background-color": "#604283"}
