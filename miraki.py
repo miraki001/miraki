0f55973a3c2d64a816d2da71ddc0fc0cefd053d7
miraki <derquicia@mariogoldsteinsa.com.ar>
2025-01-21 19:19:06 -0300

    Update miraki.py

--- miraki.py
+++ miraki.py
@@ -11,7 +11,7 @@ st.set_page_config(initial_sidebar_state="collapsed",
 st.markdown("""
         <style>
                .block-container {
-                    padding-top: 1rem;
+                    padding-top: 0rem;
                     padding-bottom: 0rem;
                     padding-left: 5rem;
                     padding-right: 5rem;
