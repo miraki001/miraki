3667ec2610ac1678d03538785c08a6c015ed8f10
miraki <derquicia@mariogoldsteinsa.com.ar>
2025-01-21 16:24:23 -0300

    Update miraki.py

--- miraki.py
+++ miraki.py
@@ -17,10 +17,12 @@ unsafe_allow_html=True)
 
 hide_menu_style = """
         <style>
-        MainMenu {visibility: hidden;}
+        #MainMenu {visibility: hidden;}
         </style>
         """
 st.markdown(hide_menu_style, unsafe_allow_html=True)
+
+clicked_menu_item = st.menu_items(['Item 1', 'Item 2', 'Item 3'], expanded=True, floating=True, emphasized_items=[0])
 
 selected = option_menu(None, ["Home", 'Novedades','Fuentes', 'Informes','Parametros','Github' ], 
         icons=['house', 'gear' ,'gear'] , menu_icon="cast",orientation="horizontal", default_index=0,
