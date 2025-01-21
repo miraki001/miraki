ced496399188a4fa2bc39fe66047f6bdb58df1c4
miraki <derquicia@mariogoldsteinsa.com.ar>
2025-01-21 15:59:01 -0300

    Update miraki.py

--- miraki.py
+++ miraki.py
@@ -6,3 +6,13 @@ import streamlit as st
 st.set_page_config(initial_sidebar_state="collapsed",
                   layout="wide",)
 
+selected = option_menu(None, ["Home", 'Ingresar','Editar' ], 
+        icons=['house', 'gear' ,'gear'] , menu_icon="cast",orientation="horizontal", default_index=0,
+        styles={
+        "container": {"padding": "0!important", "background-color": "#604283"},
+        "icon": {"color": "orange", "font-size": "14px"}, 
+        "nav-link": {"font-size": "14px", "text-align": "left", "margin":"0px", "--hover-color": "#eee"},
+        "nav-link-selected": {"background-color": "#604283"}
+        }
+  )
+
