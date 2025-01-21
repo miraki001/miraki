215bbcbd3c4abc7f0c3ed59a044e4f179fa4b2aa
miraki <derquicia@mariogoldsteinsa.com.ar>
2025-01-21 14:37:03 -0300

    Update __init__.py

--- pages/__init__.py
+++ pages/__init__.py
@@ -1,4 +1,4 @@
 from .home import show_home
 from .fuentes import show_fuentes
-#from pages.sectores import show_sectores
+from .sectores import sectores
 from .editar_fuentes import show_editar_fuentes
