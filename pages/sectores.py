95ed3aea905bc4cb0b4a83d8b31c0f289d3e63ff
miraki <derquicia@mariogoldsteinsa.com.ar>
2025-01-21 13:26:12 -0300

    Update sectores.py

--- pages/sectores.py
+++ pages/sectores.py
@@ -1,7 +1,6 @@
 import streamlit as st
 import psycopg2
 from sqlalchemy import text
-from streamlit_extras.stylable_container import stylable_container
 import os
 from streamlit_navigation_bar import st_navbar
 import pages as pg
