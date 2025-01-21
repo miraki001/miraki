7af4ba44a6ecc0b524b4d01d8176f760eed6a623
miraki <derquicia@mariogoldsteinsa.com.ar>
2025-01-21 14:45:43 -0300

    Update miraki.py

--- miraki.py
+++ miraki.py
@@ -54,8 +54,8 @@ if page == "Home":
 if page == "Informes":
    #st.switch_page("./pages/sectores.py")  
 
-"""
-functions = {
+ """
+  functions = {
     "Home": pg.show_home,
     "Seleccionar": pg.show_home,
     "Editar": pg.show_home,
@@ -63,9 +63,9 @@ functions = {
     "Informes": pg.show_home,
     "Fuentes": pg.show_fuentes,
     "Parametros": pg.show_home,
-}
-go_to = functions.get(page)
-if go_to:
+  }
+  go_to = functions.get(page)
+  if go_to:
     go_to()
 
-"""
+ """
