1e7d3e5d642adb85666534366dfea21ea0392837
miraki <derquicia@mariogoldsteinsa.com.ar>
2025-01-21 14:07:04 -0300

    Update sectores.py

--- pages/sectores.py
+++ pages/sectores.py
@@ -5,45 +5,6 @@ import os
 from streamlit_navigation_bar import st_navbar
 import pages as pg
 
-st.set_page_config(initial_sidebar_state="collapsed",
-                  layout="wide",)
-
-pages = ["Seleccionar", "Editar", "Desmarcar", "Informes", "Fuentes", "Parametros", "GitHub"]
-parent_dir = os.path.dirname(os.path.abspath(__file__))
-logo_path = os.path.join(parent_dir, "cubes.svg")
-urls = {"GitHub": "https://github.com/miraki001/miraki/blob/main/miraki.py"}
-styles = {
-    "nav": {
-        "background-color": "#604283",
-        "justify-content": "left",
-    },
-    "img": {
-        "padding-right": "14px",
-    },
-    "span": {
-        "color": "white",
-        "padding": "14px",
-    },
-    "active": {
-        "background-color": "white",
-        "color": "var(--text-color)",
-        "font-weight": "normal",
-        "padding": "14px",
-    }
-}
-options = {
-    "show_menu": False,
-    "show_sidebar": False,
-}
-
-page = st_navbar(
-    pages,
-    logo_path=logo_path,
-    urls=urls,
-    styles=styles,
-    options=options,
-    key= 14,
-)
 
 
 
