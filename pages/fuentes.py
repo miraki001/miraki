573bbad17c5be27305183bf6a2fecba8175ec3ec
miraki <derquicia@mariogoldsteinsa.com.ar>
2025-01-21 12:12:43 -0300

    Update fuentes.py

--- pages/fuentes.py
+++ pages/fuentes.py
@@ -26,7 +26,8 @@ def show_fuentes():
         "container": {"padding": "0!important", "background-color": "#fafafa"},
         "icon": {"color": "orange", "font-size": "25px"}, 
         "nav-link": {"font-size": "25px", "text-align": "left", "margin":"0px", "--hover-color": "#eee"},
-        "nav-link-selected": {"background-color": "green"})
+        "nav-link-selected": {"background-color": "green"}
+      )
 
     if select="Ingresar"
       st.switch_page("./pages/editar_fuentes.py")   
