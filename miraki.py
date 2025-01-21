60ad472a6c7fbb13d902d3702396bf52ab6c3174
miraki <derquicia@mariogoldsteinsa.com.ar>
2025-01-21 19:38:35 -0300

    Update miraki.py

--- miraki.py
+++ miraki.py
@@ -20,8 +20,7 @@ selected = option_menu(None, ["Miraki", 'Novedades','Fuentes', 'Informes','Param
         }
   )
 
-if selected=="Miraki":
-  st.switch_page("./pages/home.py")
+
 
 selected1 = option_menu(None, ["Miraki", 'Ingresar','Editar', 'Informes','Parametros','Github' ], 
         icons=['house', 'gear' ,'gear'] , menu_icon="cast",orientation="horizontal", default_index=0,
@@ -33,5 +32,3 @@ selected1 = option_menu(None, ["Miraki", 'Ingresar','Editar', 'Informes','Parame
         }
   )
 
-if selected1=="Miraki":
-  st.switch_page("./pages/home.py")
