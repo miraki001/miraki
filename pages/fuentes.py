78f5fab0b08c591d8e7af7f9a9d999ff26449a66
miraki <derquicia@mariogoldsteinsa.com.ar>
2025-01-21 12:09:36 -0300

    Update fuentes.py

--- pages/fuentes.py
+++ pages/fuentes.py
@@ -20,8 +20,13 @@ def show_fuentes():
   )
   col = st.columns((6.5, 4.5, 2), gap='medium')
   with col[0]:
-    selected = option_menu(None, ["Home", 'Settings'], 
-        icons=['house', 'gear'] , menu_icon="cast",orientation="horizontal", default_index=1)
+    selected = option_menu(None, ["Home", 'Ingresar'], 
+        icons=['house', 'gear'] , menu_icon="cast",orientation="horizontal", default_index=1
+        styles={
+        "container": {"padding": "0!important", "background-color": "#fafafa"},
+        "icon": {"color": "orange", "font-size": "25px"}, 
+        "nav-link": {"font-size": "25px", "text-align": "left", "margin":"0px", "--hover-color": "#eee"},
+        "nav-link-selected": {"background-color": "green"},)
 
 
 
