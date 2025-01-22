eb36951e42277ac81a779a89b756f65f418c760f
miraki <derquicia@mariogoldsteinsa.com.ar>
2025-01-22 10:05:26 -0300

    Update parametros.py

--- pages/parametros.py
+++ pages/parametros.py
@@ -1,1 +1,55 @@
+import streamlit as st
+import psycopg2
+from sqlalchemy import text
+#from streamlit_extras.stylable_container import stylable_container
 
+
+col41, mid, col42 = st.columns([1,1,20])
+with col41:
+    st.image('ic_launcher44.png', width=60)
+with col42:
+    st.title('Miraki')
+
+st.subheader("Plataforma de Vigilancia Tecnólogica e Inteligencia Competitiva")
+
+
+col1, col2, col3,col4,col5,col6,col7 = st.columns(7)
+
+tnuri = 0
+vtitulo= ''
+vdetalle = ''
+vlink = ''
+vimagen = ''
+
+
+
+st.markdown("""
+            <style>
+            div.stButton {text-align:center}
+            div.stButton > button:first-child {
+                background-color: #b579c2;
+                color:#000000;
+                font-weight: bold;
+            }
+            div.stButton > button:hover {
+                background-color: #79adc2;
+                color:#ff0000;
+                }
+            </style>""", unsafe_allow_html=True)
+
+
+
+if col1.button("Volver" ,  type='primary'):
+    st.switch_page("streamlit_app.py")
+if col2.button("Palabras Claves"):
+    st.switch_page("./pages/palabrasclaves.py")
+if col3.button("Excluidas"):
+    st.switch_page("./pages/editar_fuentes.py")
+if col4.button("Sectores"):
+    st.switch_page("./pages/sectores.py")   
+if col5.button("Ejes"):
+    st.switch_page("./pages/ejes.py")
+if col6.button("Palabras por Sector"):
+    st.switch_page("./pages/scraptodo.py")
+if col7.button("Proyectos"):
+    st.switch_page("./pages/proyectos.py")
