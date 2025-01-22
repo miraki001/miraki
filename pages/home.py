12cddcb58b9238c7b997268f6904302574e79dd7
miraki <derquicia@mariogoldsteinsa.com.ar>
2025-01-22 12:37:16 -0300

    Update home.py

--- pages/home.py
+++ pages/home.py
@@ -2,26 +2,41 @@ import streamlit as st
 from streamlit_option_menu import option_menu
 
 st.set_page_config(layout="wide")
-# Custom HTML/CSS for the banner
-custom_html = """
-<div class="banner">
-    <img src="https://img.freepik.com/premium-photo/wide-banner-with-many-random-square-hexagons-charcoal-dark-black-color_105589-1820.jpg" alt="Banner Image">
-</div>
-<style>
-    .banner {
-        width: 100%;
-        height: 20px;
-        overflow: hidden;
-    }
-    .banner img {
-        width: 100%;
-        object-fit: cover;
-    }
-</style>
-"""
-# Display the custom HTML
-st.components.v1.html(custom_html)
 
+st.markdown("""
+<style>
+:root {
+  --header-height: 50px;
+  --header-height-padded: 59px;
+}
+
+[data-testid="stHeader"] {
+    background-image: url(/app/static/icon.png);
+    background-repeat: no-repeat;
+    background-size: contain;
+    background-orgin: content-box;
+    background-color: grey;
+    padding-top: var(--header-height);
+}
+
+[data-testid="stSidebar"] {
+    margin-top: var(--header-height-padded);
+}
+
+[data-testid="stSidebarCollapsedControl"] {
+    margin-top: var(--header-height);
+}
+
+[data-testid="stSidebarUserContent"] {
+    padding-top: 0rem;
+}
+</style>
+""", unsafe_allow_html=True)
+
+with st.sidebar:
+    st.markdown("sidebar")
+
+st.markdown("main")
 
 
 selected2 = option_menu(None, ["Miraki", 'Novedades','Fuentes', 'Informes','Parametros','Github' ], 
