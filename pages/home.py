c2056376b44cd91df9387406e0ccb71d81855f3a
miraki <derquicia@mariogoldsteinsa.com.ar>
2025-01-22 13:34:03 -0300

    Update home.py

--- pages/home.py
+++ pages/home.py
@@ -69,17 +69,6 @@ st.write(
         
         Plataforma de Vigilancia Tecnologica e Inteligencia Competitiva.
         
-        ornare. Morbi id ex pulvinar dui placerat congue. Suspendisse ultricies, lacus
-        eget porttitor blandit, enim nisi tincidunt eros, nec varius tortor turpis et
-        tortor.
-
-        Curabitur facilisis, augue eu eleifend dictum, quam lectus ullamcorper tellus,
-        auctor mollis lacus turpis id tellus. Mauris consectetur eleifend dignissim.
-        Integer nulla arcu, fringilla quis finibus vel, iaculis ac massa. Cras at
-        mauris a magna blandit mattis. Nam vel turpis et risus tempus congue ac quis
-        lectus. Pellentesque id laoreet ex, sit amet consequat leo. Aenean commodo
-        luctus tristique. Curabitur arcu urna, tempus ut lectus et, pulvinar lobortis
-        urna.
     """
 )
 
