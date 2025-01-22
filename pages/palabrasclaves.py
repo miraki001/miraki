5834ce6d13ada87277acf7c215bae7d36415785f
miraki <derquicia@mariogoldsteinsa.com.ar>
2025-01-22 15:54:41 -0300

    Update palabrasclaves.py

--- pages/palabrasclaves.py
+++ pages/palabrasclaves.py
@@ -1,1 +1,85 @@
+import streamlit as st
+import psycopg2
+from sqlalchemy import text
 
+
+
+tnuri = 0
+vtitulo= ''
+vdetalle = ''
+vlink = ''
+vimagen = ''
+
+
+
+st.markdown("""
+            <style>
+            div.stButton {text-align:center}
+            div.stButton > button:first-child {
+                background-color: #b579c2;
+                color:#000000;
+                font-weight: bold;
+            }
+            div.stButton > button:hover {
+                background-color: #79adc2;
+                color:#ff0000;
+                }
+            </style>""", unsafe_allow_html=True)
+
+
+
+
+
+
+conn = st.connection("postgresql", type="sql")
+qq = 'select palabra,peso from palabras_a_buscar  ;'
+df1 = conn.query(qq, ttl="0"),
+df = df1[0]
+
+
+
+ppalabra = st.text_input("ingrese el nombre de la palabra")
+
+if ppalabra:
+    mask = df.applymap(lambda x: ppalabra in str(x).lower()).any(axis=1)
+    df = df[mask]
+
+
+
+colors = ['red', 'orange', 'yellow', 'green', 'blue', 'indigo', 'violet']
+config = {
+    'palabra' : st.column_config.TextColumn('palabra', required=True),
+    'peso' : st.column_config.NumberColumn('peso',),
+
+
+    
+}
+#result = st.data_editor(df, column_config = config, num_rows='dynamic')
+def dataframe_with_selections(df):
+                    df_with_selections = df.copy()
+                    df_with_selections.insert(0, "Selec", False)
+                    # Get dataframe row-selections from user with st.data_editor
+                    edited_df = st.data_editor(
+                        df_with_selections,
+                        hide_index=True,
+                        column_config=
+                        {"Select": st.column_config.CheckboxColumn(required=True),
+                        'url' : st.column_config.LinkColumn('palabra'),      
+                        },
+                        disabled=df.columns,
+#                        num_rows="dynamic",
+                    )
+
+                    # Filter the dataframe using the temporary column, then drop the column
+                    selected_rows = edited_df[edited_df.Selec]
+                    return selected_rows.drop('Selec', axis=1)
+                  
+selection = dataframe_with_selections(df)
+
+cnt = len(selection)
+if cnt > 0:
+            vpalabra = selection.to_string(columns=['palabra'], header=False, index=False)
+            vpeso = selection.to_string(columns=['peso'], header=False, index=False)
+            st.write(vpalabra)
+            st.session_state['vpalabra'] = vpalabra
+            st.session_state['vpeso'] = vpeso
