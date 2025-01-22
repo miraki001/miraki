bb954b39c1b81cee619109885223335769df26ef
miraki <derquicia@mariogoldsteinsa.com.ar>
2025-01-22 16:05:55 -0300

    Update seleccionar.py

--- pages/seleccionar.py
+++ pages/seleccionar.py
@@ -1,1 +1,25 @@
+import streamlit as st
+import psycopg2
+from sqlalchemy import text
 
+tnuri = st.session_state['vnuri']
+st.write(tnuri)
+conn = st.connection("postgresql", type="sql")
+
+actualizar = 'update novedades set select_web = :estado,nro_reporte = 0 where nuri = ' + tnuri + ';'
+nuri = tnuri
+new = 'S'
+st.write(actualizar)
+
+
+with conn.session as session:
+    #session.execute(f"UPDATE novedades SET select_web = '{new}' WHERE nuri='{nuri}'")
+    session.execute(text("UPDATE novedades SET select_web = :val, nro_reporte = 0 WHERE nuri= :nuri"), {"val": new,"nuri": nuri})
+                        
+    session.commit()
+    st.success("Data sent")
+
+st.switch_page("streamlit_app.py")
+
+
+#df1 = conn.execute(actualizar, ttl="0",params={"estado": "S"} ),
