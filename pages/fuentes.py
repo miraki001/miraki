ba3e5de9a6a696b62653482bab6745ca6068d0d6
miraki <derquicia@mariogoldsteinsa.com.ar>
2025-01-22 08:31:39 -0300

    Update fuentes.py

--- pages/fuentes.py
+++ pages/fuentes.py
@@ -19,13 +19,13 @@ st.markdown(
     """,
   unsafe_allow_html=True,
 )
-
+#eee
 selected4 = option_menu(None, ["Home1", 'Ingresar','Editar','Borrar','Duplicar','Verificar','Analizar' ], 
       icons=['house', 'gear' ,'gear'] , menu_icon="cast",orientation="horizontal", default_index=-1,
       styles={
         "container": {"padding": "0!important", "background-color": "#604283"},
         "icon": {"color": "orange", "font-size": "14px"}, 
-        "nav-link": {"font-size": "14px", "text-align": "left", "margin":"0px", "--hover-color": "#eee"},
+        "nav-link": {"font-size": "14px", "text-align": "left", "margin":"0px", "--hover-color": "white"},
         "nav-link-selected": {"background-color": "#604283"}
       }
 )
