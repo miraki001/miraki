d763191815074160d22be07d548aefc0609593db
miraki <derquicia@mariogoldsteinsa.com.ar>
2025-01-22 12:31:11 -0300

    Update home.py

--- pages/home.py
+++ pages/home.py
@@ -1,6 +1,27 @@
 import streamlit as st
 from streamlit_option_menu import option_menu
 
+st.set_page_config(layout="wide")
+# Custom HTML/CSS for the banner
+custom_html = """
+<div class="banner">
+    <img src="https://img.freepik.com/premium-photo/wide-banner-with-many-random-square-hexagons-charcoal-dark-black-color_105589-1820.jpg" alt="Banner Image">
+</div>
+<style>
+    .banner {
+        width: 160%;
+        height: 200px;
+        overflow: hidden;
+    }
+    .banner img {
+        width: 100%;
+        object-fit: cover;
+    }
+</style>
+"""
+# Display the custom HTML
+st.components.v1.html(custom_html)
+
 
 
 selected2 = option_menu(None, ["Miraki", 'Novedades','Fuentes', 'Informes','Parametros','Github' ], 
