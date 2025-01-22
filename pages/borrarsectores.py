dfc0dbf6f1ddd44e640a935e6a673bd79d19b100
miraki <derquicia@mariogoldsteinsa.com.ar>
2025-01-22 16:04:06 -0300

    Update borrarsectores.py

--- pages/borrarsectores.py
+++ pages/borrarsectores.py
@@ -1,1 +1,14 @@
+import streamlit as st
+import psycopg2
+from sqlalchemy import text
 
+st.set_page_config(layout="wide")
+conn = st.connection("postgresql", type="sql")
+
+tnuri = st.session_state['vnuri']
+
+with conn.session as session:
+  actualiza = 'delete from sectores   where nuri = ' +  tnuri
+  session.execute(text(actualiza) )
+  session.commit()
+st.switch_page("./pages/sectores.py")
