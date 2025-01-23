a068a0cac61b4d307a65eca88f9603006d93a449
miraki <derquicia@mariogoldsteinsa.com.ar>
2025-01-23 19:42:02 -0300

    Update novedades.py

--- pages/novedades.py
+++ pages/novedades.py
@@ -76,7 +76,7 @@ event = st.dataframe(
         column_config=config,
         use_container_width=True,
         hide_index=True,
-        on_select="callable",
+        on_select="rerun",
         selection_mode="single-row",
     )
 
