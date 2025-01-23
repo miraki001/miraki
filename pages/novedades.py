d03d2eb76a6498c3c384d60956780a9b95568c9c
miraki <derquicia@mariogoldsteinsa.com.ar>
2025-01-23 15:03:05 -0300

    Update novedades.py

--- pages/novedades.py
+++ pages/novedades.py
@@ -72,7 +72,7 @@ st.write('este es resultado antes')
 result = st.data_editor(df, column_config = config, num_rows='dynamic')
 #favorite_command = result.loc[result[“nuri”].idxmax()][“command”]
 #st.markdown(f"Your favorite command is {favorite_command} :balloon:")
-st.write(result[“nuri”].idxmax())
+st.write(result["nuri"].idxmax())
 
 
 st.write('este es resultado despues')
