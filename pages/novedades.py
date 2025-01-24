6dd4574938271cc02c9f7c6828f2799f3eba4c2f
miraki <derquicia@mariogoldsteinsa.com.ar>
2025-01-24 16:15:26 -0300

    Update novedades.py

--- pages/novedades.py
+++ pages/novedades.py
@@ -163,7 +163,7 @@ def dataframe_with_selections(df):
                     edited_df = st.data_editor(
                         df_with_selections,
                         hide_index=False,
-                        key='_df',
+                        #key='_df',
                         #on_change=store_df, args=['df'],
                         #style.apply(lambda x: ["background: red" if v > x.iloc[0] else "" for v in x], axis = 1),
                         use_container_width=True,
