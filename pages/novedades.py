cf2d478b7107a8cf4df5f4158fae3be8b48c12b5
miraki <derquicia@mariogoldsteinsa.com.ar>
2025-01-24 13:27:21 -0300

    Update novedades.py

--- pages/novedades.py
+++ pages/novedades.py
@@ -47,6 +47,35 @@ vdetalle = ''
 vlink = ''
 vimagen = ''
 
+""" aca
+
+selection = dataframe_with_selections(df)
+
+selected241 = option_menu(None, ["Home", 'Ingresar','Editar','Borrar','Seleccionar','Desmarcar' ], 
+      icons=['house', 'plus' ,'pencil-square','eraser','chek','patch-chek'] , menu_icon="cast",orientation="horizontal", default_index=-1,
+                
+      styles={
+        "container": {"padding": "0!important", "background-color": "#604283"},
+        "icon": {"color": "orange", "font-size": "14px"}, 
+        "nav-link": {"color": "white", "font-size": "14px", "text-align": "left", "margin":"0px", "--hover-color": "#B3D3F0"},
+        "nav-link-selected": {"background-color": "#604283"}
+      }
+)
+
+
+if selected241=="Home":
+    st.switch_page("miraki.py") 
+if selected241=="Ingresar":
+    st.session_state['vTipo'] = 'Ingresar'
+    st.switch_page("./pages/editarnovedades.py")   
+if selected241=="Editar":
+    st.session_state['vTipo'] = 'Editar'
+    st.switch_page("./pages/editarnovedades.py") 
+if selected241=="Seleccionar":
+    seleccionar(df)
+if selected241=="Desmarcar":
+    desmarcar(df)
+
 
  
 
@@ -109,33 +138,7 @@ def dataframe_with_selections(df):
                     selected_rows = edited_df[edited_df.Selec]
                     return selected_rows.drop('Selec', axis=1)
                   
-selection = dataframe_with_selections(df)
-
-selected241 = option_menu(None, ["Home", 'Ingresar','Editar','Borrar','Seleccionar','Desmarcar' ], 
-      icons=['house', 'plus' ,'pencil-square','eraser','chek','patch-chek'] , menu_icon="cast",orientation="horizontal", default_index=-1,
-                
-      styles={
-        "container": {"padding": "0!important", "background-color": "#604283"},
-        "icon": {"color": "orange", "font-size": "14px"}, 
-        "nav-link": {"color": "white", "font-size": "14px", "text-align": "left", "margin":"0px", "--hover-color": "#B3D3F0"},
-        "nav-link-selected": {"background-color": "#604283"}
-      }
-)
-
-
-if selected241=="Home":
-    st.switch_page("miraki.py") 
-if selected241=="Ingresar":
-    st.session_state['vTipo'] = 'Ingresar'
-    st.switch_page("./pages/editarnovedades.py")   
-if selected241=="Editar":
-    st.session_state['vTipo'] = 'Editar'
-    st.switch_page("./pages/editarnovedades.py") 
-if selected241=="Seleccionar":
-    seleccionar(df)
-if selected241=="Desmarcar":
-    desmarcar(df)
-
+"""aca
 
 
 
