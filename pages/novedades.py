d05e03e0be647c99f5bb85b6d64b010c2ff48e98
miraki <derquicia@mariogoldsteinsa.com.ar>
2025-01-24 10:57:43 -0300

    Update novedades.py

--- pages/novedades.py
+++ pages/novedades.py
@@ -82,7 +82,7 @@ event = st.dataframe(
         selection_mode="single-row",
     )
 
-edited_df = st.data_editor(df, column_config=config,num_rows="dynamic")
+edited_df = st.data_editor(df, column_config=config,use_container_width=True,hide_index=True,num_rows="dynamic")
 #st.header("Selected members")
 people = event.selection.rows
 #st.write(people)
