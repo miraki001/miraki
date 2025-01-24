050edca6ed25c42a1c2880bfa95236faadd1ccbd
miraki <derquicia@mariogoldsteinsa.com.ar>
2025-01-24 15:27:17 -0300

    Update novedades.py

--- pages/novedades.py
+++ pages/novedades.py
@@ -119,7 +119,7 @@ def dataframe_with_selections(df):
 
                     # Filter the dataframe using the temporary column, then drop the column
                     #edited_df = edited_df[edited_df.nuri == 461048]
-                    selected_rows = edited_df[edited_df.Select]
+                    selected_rows = edited_df[edited_df.Selec]
                     return selected_rows.drop('Selec', axis=1)
 
 
