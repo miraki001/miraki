e83f3a22cc2201c765dfafac964a3f9666a42b1c
miraki <derquicia@mariogoldsteinsa.com.ar>
2025-01-24 13:15:18 -0300

    Update novedades.py

--- pages/novedades.py
+++ pages/novedades.py
@@ -146,7 +146,7 @@ if selected241=="Desmarcar":
 st.write(selection)
 
 #selection  =df.iloc[people]
-vv=  selection[-1]
+vv= df.iloc[selection]  
 st.write(vv)
 #st.session_state['recno'] =  people[0]
 cnt = len(selection)
