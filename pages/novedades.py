d47faba2ab10b4ce01629aa380316728ac5141cf
miraki <derquicia@mariogoldsteinsa.com.ar>
2025-01-25 10:37:33 -0300

    Update novedades.py

--- pages/novedades.py
+++ pages/novedades.py
@@ -125,7 +125,7 @@ st.write(people)
 
 selection  =df.iloc[people]
 st.write(selection.index[0])
-#st.session_state['recno'] =  people[0]
+st.session_state['recno'] =  people[0]
 cnt = len(selection)
 
 if cnt>0:
