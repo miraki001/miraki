1becb6eb154b976d150a6b21002bab4e42d1d8ad
miraki <derquicia@mariogoldsteinsa.com.ar>
2025-01-25 18:25:55 -0300

    Update editarnovedades.py

--- pages/editarnovedades.py
+++ pages/editarnovedades.py
@@ -18,7 +18,7 @@ df = df1[0]
 st.write(df)
 #conn1 = st.connection("postgresql", type="sql")
 #vquery = "select nuri,eje from ejestemas where eje = :eje  ;"
-df234 = conn1.query('select nuri,eje from ejestemas ; ', ttl="0"),
+df234 = conn.query('select nuri,eje from ejestemas ; ', ttl="0"),
 st.write(df234)
 
 
