56dea990683fc0194358fc893d78fc97978b54bc
miraki <derquicia@mariogoldsteinsa.com.ar>
2025-01-25 18:56:17 -0300

    Update editarnovedades.py

--- pages/editarnovedades.py
+++ pages/editarnovedades.py
@@ -20,6 +20,13 @@ st.write(df)
 #conn1 = st.connection("postgresql", type="sql")
 #vquery = "select nuri,eje from ejestemas where eje = :eje  ;"
 #df234 = conn.query('select nuri,eje from ejestemas ; ', ttl="0"),
+
+cursor = conn.cursor()
+cursor.execute('select nuri,eje from ejestemas ;')
+tuples_list = cursor.fetchall()
+cursor.close()
+df2 = pd.DataFrame(tuples_list, columns=column_names)
+st.write(df2)
 df11 = conn.query('select nuri,eje from ejestemas ;', ttl="0"),
 st.write(df11)
 #columns = [desc[0] for desc in df1.description] 
