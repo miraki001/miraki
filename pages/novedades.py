6039010beaf228f225a5e7a3e9c246b2a4d91600
miraki <derquicia@mariogoldsteinsa.com.ar>
2025-01-26 09:24:08 -0300

    Update novedades.py

--- pages/novedades.py
+++ pages/novedades.py
@@ -23,8 +23,8 @@ st.markdown(
                .block-container {
                     padding-top: 0rem;
                     padding-bottom: 0rem;
-                    padding-left: 5rem;
-                    padding-right: 5rem;
+                    padding-left: 2rem;
+                    padding-right: 2rem;
                 }
         </style>
         """,
