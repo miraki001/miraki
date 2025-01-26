398a3c390cd84704c298ada321f7134cc5440045
miraki <derquicia@mariogoldsteinsa.com.ar>
2025-01-26 09:02:28 -0300

    Update novedades.py

--- pages/novedades.py
+++ pages/novedades.py
@@ -10,8 +10,22 @@ st.set_page_config(initial_sidebar_state="collapsed",
                   layout="wide",menu_items=None,page_title="Miraki")
 
 
-st.markdown("""
+
+kavyajeetbora
+
+VirtuaPT
+Dec 2024
+Thanks very much for this. It perfectly works for me.
+Additionally you can add one more line to make the header transparant if required, helps while scrolling:
+
+st.markdown(
+    """
         <style>
+                .stAppHeader {
+                    background-color: rgba(255, 255, 255, 0.0);  /* Transparent background */
+                    visibility: visible;  /* Ensure the header is visible */
+                }
+
                .block-container {
                     padding-top: 0rem;
                     padding-bottom: 0rem;
@@ -19,7 +33,10 @@ st.markdown("""
                     padding-right: 5rem;
                 }
         </style>
-        """, unsafe_allow_html=True)
+        """,
+    unsafe_allow_html=True,
+)
+
 
 #vnuri = st.session_state['vnuri']
 #st.session_state.vnuri = 0
