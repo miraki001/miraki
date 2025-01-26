baa0c370fae8e3abec3327e6154ccca29d9356cc
miraki <derquicia@mariogoldsteinsa.com.ar>
2025-01-26 08:34:50 -0300

    Update novedades.py

--- pages/novedades.py
+++ pages/novedades.py
@@ -9,6 +9,40 @@ import numpy as np
 st.set_page_config(initial_sidebar_state="collapsed",
                   layout="wide",menu_items=None,page_title="Miraki")
 
+
+hide_streamlit_style = """
+                <style>
+                div[data-testid="stToolbar"] {
+                visibility: hidden;
+                height: 0%;
+                position: fixed;
+                }
+                div[data-testid="stDecoration"] {
+                visibility: hidden;
+                height: 0%;
+                position: fixed;
+                }
+                div[data-testid="stStatusWidget"] {
+                visibility: hidden;
+                height: 0%;
+                position: fixed;
+                }
+                #MainMenu {
+                visibility: hidden;
+                height: 0%;
+                }
+                header {
+                visibility: hidden;
+                height: 0%;
+                }
+                footer {
+                visibility: hidden;
+                height: 0%;
+                }
+                </style>
+                """
+st.markdown(hide_streamlit_style, unsafe_allow_html=True)
+
 #vnuri = st.session_state['vnuri']
 #st.session_state.vnuri = 0
 st.subheader("Novedades")
