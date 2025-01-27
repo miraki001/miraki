e3bf9986343e81a70c8997286a2271413f85f144
miraki <derquicia@mariogoldsteinsa.com.ar>
2025-01-27 12:01:21 -0300

    Update ejes.py

--- pages/ejes.py
+++ pages/ejes.py
@@ -45,7 +45,7 @@ def borrar():
 
 
 selected41 = option_menu(None, ["Home", 'Ingresar','Editar','Borrar' ], 
-      icons=['house', 'plus' ,'pencil-square','eraser'] , menu_icon="cast",orientation="horizontal", default_index=-1,
+      icons=['house', 'plus' ,'pencil-square','eraser'] , menu_icon="cast",orientation="horizontal", default_index=-8,
                 
       styles={
         "container": {"padding": "0!important", "background-color": "#604283"},
