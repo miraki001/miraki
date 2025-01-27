8bdc0d2426f9fda06acdad4ac36c7c1f42858954
miraki <derquicia@mariogoldsteinsa.com.ar>
2025-01-27 08:42:39 -0300

    Update proyectos.py

--- pages/proyectos.py
+++ pages/proyectos.py
@@ -3,9 +3,36 @@ import psycopg2
 from sqlalchemy import text
 from streamlit_option_menu import option_menu
 
-st.set_page_config(initial_sidebar_state="collapsed",
-                  layout="wide",menu_items=None ,page_title="Miraki")
 
+st.set_page_config(initial_sidebar_state="collapsed",
+                  layout="wide",menu_items=None,page_title="Miraki")
+
+
+st.markdown(
+    """
+        <style>
+                .stAppHeader {
+                    background-color: rgba(255, 255, 255, 0.0);  /* Transparent background */
+                    background-image: url(http://placekitten.com/200/200);
+                    background-position: 20px 20px;
+                    visibility: visible;  /* Ensure the header is visible */
+                }
+
+               .block-container {
+                    padding-top: 0rem;
+                    padding-bottom: 0rem;
+                    padding-left: 2rem;
+                    padding-right: 2rem;
+                }
+        </style>
+        """,
+    unsafe_allow_html=True,
+)
+
+
+#vnuri = st.session_state['vnuri']
+#st.session_state.vnuri = 0
+st.subheader("Miraki - Proyectos")
 
 selected6 = option_menu(None, ["Home", 'Ingresar','Editar','Borrar'], 
       icons=['house', 'plus' ,'pencil-square','eraser'] , menu_icon="cast",orientation="horizontal", default_index=-1,
