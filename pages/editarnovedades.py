bbaa42310fa4719a8bff1e9f3b72dd6622ac4a51
miraki <derquicia@mariogoldsteinsa.com.ar>
2025-01-27 14:54:27 -0300

    Update editarnovedades.py

--- pages/editarnovedades.py
+++ pages/editarnovedades.py
@@ -7,6 +7,35 @@ import pandas as pd
 st.set_page_config(initial_sidebar_state="collapsed",
                   layout="wide",menu_items=None,page_title="Miraki")
 
+
+
+st.markdown(
+    """
+        <style>
+                .stAppHeader {
+                    background-color: rgba(255, 255, 255, 0.0);  /* Transparent background */
+                    background-image: url(http://placekitten.com/200/200);
+                    background-position: 20px 20px;
+                    visibility: visible;  /* Ensure the header is visible */
+                }
+
+               .block-container {
+                    padding-top: 0rem;
+                    padding-bottom: 0rem;
+                    padding-left: 2rem;
+                    padding-right: 2rem;
+                }
+        </style>
+        """,
+    unsafe_allow_html=True,
+)
+
+
+#vnuri = st.session_state['vnuri']
+#st.session_state.vnuri = 0
+st.subheader("Miraki - Novedades")
+
+
 vtitulo = st.session_state['vtitulo']
 veje1 = st.session_state['veje']
 votro_nuri = st.session_state['vnuri1']
