92c6c913d9064704860970d5dba3698e88c20228
miraki <derquicia@mariogoldsteinsa.com.ar>
2025-01-27 14:28:26 -0300

    Update palabrasclaves.py

--- pages/palabrasclaves.py
+++ pages/palabrasclaves.py
@@ -35,7 +35,7 @@ def borrar():
   conn = st.connection("postgresql", type="sql")
   tpalabra = st.session_state['vpalabra']
   with conn.session as session:
-    actualiza = 'delete from palabras_a_buscar   where palabra = tpalabra ;'
+    actualiza = 'delete from palabras_a_buscar   where palabra = :palabra ;'
     session.execute(text(actualiza), {"palabra": tpalabra})
     session.execute(text(actualiza) )
     session.commit()
