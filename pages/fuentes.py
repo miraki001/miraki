f5eb29711f98cc15186fbb7c9f7f189c0cd9e897
miraki <derquicia@mariogoldsteinsa.com.ar>
2025-01-27 14:03:43 -0300

    Update fuentes.py

--- pages/fuentes.py
+++ pages/fuentes.py
@@ -38,6 +38,13 @@ def borrar():
       session.commit()
 
 def duplicar():
+  conn = st.connection("postgresql", type="sql")
+  with conn.session as session:
+    actualiza = "INSERT INTO fuentes_py( nuri, fuente, fecha_act, activa, tipo, descrip, proyecto_nuri, limite, tema, busqueda_pers, pais, idioma, separador, xpath_titulo, xpath_detalle, xpath_autores, sep_autores, cnt_noticias, utfdecode, xpath_publica, act_url_fecha, urlencode, cnt_encontradas, baja_def, observa, cod_pais, xpath_link, xpath_image, atributo1, atributo2, tipo_busq, fuente_org, urllink, posjson)"
+    actualiza = actualiza  + " select nextval('fuentes_py_seq')  , fuente, fecha_act, activa, tipo, descrip, proyecto_nuri, limite, tema, busqueda_pers, pais, idioma, separador, xpath_titulo, xpath_detalle, xpath_autores, sep_autores, cnt_noticias, utfdecode, xpath_publica, act_url_fecha, urlencode, cnt_encontradas, baja_def, observa, cod_pais, xpath_link, xpath_image, atributo1, atributo2, tipo_busq, fuente_org, urllink, posjson  " 
+    actualiza = actualiza + " from fuentes_py where nuri = :nuri ;"
+    session.execute(text(actualiza), {"nuri": tnuri})
+    session.commit()  
 
 #vnuri = st.session_state['vnuri']
 #st.session_state.vnuri = 0
