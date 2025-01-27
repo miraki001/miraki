a4f72394a5bdd477483e8aee74684ba4321d68bb
miraki <derquicia@mariogoldsteinsa.com.ar>
2025-01-27 12:26:24 -0300

    Update ejes.py

--- pages/ejes.py
+++ pages/ejes.py
@@ -45,7 +45,7 @@ def borrar():
 miindex = -1
 selected41 =''
 
-selected41 = option_menu(None, ["Home", 'Ingresar','Editar','Borrar' ], 
+selected41 = option_menu(None, ["Home", "Ingresar","Editar","Borrar" ], 
       icons=['house', 'plus' ,'pencil-square','eraser'] , menu_icon="cast",orientation="horizontal", default_index=miindex,
                 
       styles={
