649d54894868e4ec1c14688852eea9ebfe8c54b1
miraki <derquicia@mariogoldsteinsa.com.ar>
2025-01-27 14:44:29 -0300

    Update ingproyectos.py

--- pages/ingproyectos.py
+++ pages/ingproyectos.py
@@ -2,6 +2,40 @@ import streamlit as st
 import psycopg2
 from sqlalchemy import text
 
+st.set_page_config(initial_sidebar_state="collapsed",
+                  layout="wide",menu_items=None,page_title="Miraki")
+
+
+
+
+st.markdown(
+    """
+        <style>
+                .stAppHeader {
+                    background-color: rgba(255, 255, 255, 0.0);  /* Transparent background */
+                    background-image: url(http://placekitten.com/200/200);
+                    background-position: 20px 20px;
+                    visibility: visible;  /* Ensure the header is visible */
+                }
+
+               .block-container {
+                    padding-top: 0rem;
+                    padding-bottom: 0rem;
+                    padding-left: 2rem;
+                    padding-right: 2rem;
+                }
+        </style>
+        """,
+    unsafe_allow_html=True,
+)
+
+
+#vnuri = st.session_state['vnuri']
+#st.session_state.vnuri = 0
+st.subheader("Miraki - Proyectos")
+
+
+
 
 def actualizar():
     conn = st.connection("postgresql", type="sql")
