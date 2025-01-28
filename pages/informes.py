d1c432808a66f372b99e2d31ef576af6e8e8cd8b
miraki <derquicia@mariogoldsteinsa.com.ar>
2025-01-28 14:29:15 -0300

    Update informes.py

--- pages/informes.py
+++ pages/informes.py
@@ -27,7 +27,7 @@ st.markdown(
         <style>
                 .stAppHeader {
                     background-color: rgba(255, 255, 255, 0.0);  /* Transparent background */
-                    background-image: url(gs://miraki-7ca50.appspot.com/android-chrome-512x512.png);
+                    background-image: url(https://firebasestorage.googleapis.com/v0/b/miraki-7ca50.appspot.com/o/android-chrome-512x512.png?alt=media&token=8494362c-08da-443a-8545-ed2ab5243284);
                     background-position: 20px 20px;
                     visibility: visible;  /* Ensure the header is visible */
                 }
