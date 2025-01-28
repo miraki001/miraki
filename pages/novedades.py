f62869db1b1233021eac69902a80746c8a2b7860
miraki <derquicia@mariogoldsteinsa.com.ar>
2025-01-28 14:57:57 -0300

    Update novedades.py

--- pages/novedades.py
+++ pages/novedades.py
@@ -33,7 +33,10 @@ st.markdown(
     unsafe_allow_html=True,
 )
 
-
+st.logo(
+    "https://firebasestorage.googleapis.com/v0/b/miraki-7ca50.appspot.com/o/android-chrome-192x192.png?alt=media&token=e8d5f6ee-706a-4399-813d-fd82f8b35ec7",
+    icon_image="https://firebasestorage.googleapis.com/v0/b/miraki-7ca50.appspot.com/o/android-chrome-192x192.png?alt=media&token=e8d5f6ee-706a-4399-813d-fd82f8b35ec7",
+)
 #vnuri = st.session_state['vnuri']
 #st.session_state.vnuri = 0
 st.subheader("Miraki - Novedades")
