335b07a5c2364ddd63c1db6e8906a2344e4e0c3e
miraki <derquicia@mariogoldsteinsa.com.ar>
2025-01-28 12:45:18 -0300

    Update informes.py

--- pages/informes.py
+++ pages/informes.py
@@ -118,7 +118,7 @@ with tab2:
     st.header("Tendencias")
     conn = st.connection("postgresql", type="sql")
     df = conn.query('select palabra as text,cnt as value from tag_words() ;', ttl="0")
-    df.to_dict('words')
+    words = df.to_dict('series')
     #words = [{"text": "Python", "value": 500, "topic": "lol"}, {"text": "Streamlit", "value": 80},{"text": "Streamlit", "value": 80},{"text": "Streamlit", "value": 80},{"text": "Streamlit", "value": 80},{"text": "Streamlit", "value": 80},{"text": "Streamlit", "value": 80}]
 
     st_wordcloud(words, width=800, height=600)
