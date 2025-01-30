9f99386d77b89e2a137e6a73076d08ae936fce21
miraki <derquicia@mariogoldsteinsa.com.ar>
2025-01-30 19:44:47 -0300

    Update verificar.py

--- pages/verificar.py
+++ pages/verificar.py
@@ -116,8 +116,8 @@ if tipobusq != 'json':
     html_content = response.content
     tree = html.fromstring(html_content)
     soup = BeautifulSoup(html_content, 'lxml')
-    noticias = soup.find_all(string=re.compile("dg_news_hl_news_"))
-    st.write(noticias)
+    #noticias = soup.find_all(string=re.compile("dg_news_hl_news_"))
+    #st.write(noticias)
     if vatrib1 != '':
         noticias = soup.find_all(separador,newv)
     if vatrib1 == '':    
