65ef4356e884849a14a63fa0711aec454f1be933
miraki <derquicia@mariogoldsteinsa.com.ar>
2025-01-30 13:26:44 -0300

    Update verificar.py

--- pages/verificar.py
+++ pages/verificar.py
@@ -49,7 +49,7 @@ if p != 0:
     atr1 = resto[:p]
     atr1 = atr1[1:100]
     st.write(atr1)
-    atr2 = resto[p+1:100]
+    atr2 = resto[p+1:len(atr2)-|]
     st.write(atr2)
 
 a = xtitulo.strip("{}")
