cc23718461a26c4c18883946a63b5d8ce0386b6d
miraki <derquicia@mariogoldsteinsa.com.ar>
2025-01-31 11:15:18 -0300

    Update informes.py

--- pages/informes.py
+++ pages/informes.py
@@ -181,17 +181,8 @@ with tab3:
         HtmlFile = open(f'{path}/pyvis_graph.html', 'r', encoding='utf-8')
 
     # Load HTML file in HTML component for display on Streamlit page
-    components.html(HtmlFile.read(), height=435)
+    components.html(HtmlFile.read(), height=635)
 
-# Footer
-st.markdown(
-    """
-    <br>
-    <h6><a href="https://github.com/kennethleungty/Pyvis-Network-Graph-Streamlit" target="_blank">GitHub Repo</a></h6>
-    <h6><a href="https://kennethleungty.medium.com" target="_blank">Medium article</a></h6>
-    <h6>Disclaimer: This app is NOT intended to provide any form of medical advice or recommendations. Please consult your doctor or pharmacist for professional advice relating to any drug therapy.</h6>
-    """, unsafe_allow_html=True
-    )
 
 
 
