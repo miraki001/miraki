a1610c2e0225c8e933f8ed5622fec34dbb60f28e
miraki <derquicia@mariogoldsteinsa.com.ar>
2025-01-31 14:32:00 -0300

    Update verificar.py

--- pages/verificar.py
+++ pages/verificar.py
@@ -99,7 +99,7 @@ df1 = conn.query(qq, ttl="0"),
 st.write(df1[0])
 
 
-tira= 'Studies sedes aire plagas stress on the preparation of a sufficient carrier from egg protein and carrageenan for cellulase with optimization and application'
+#tira= 'Studies sedes aire plagas stress on the preparation of a sufficient carrier from egg protein and carrageenan for cellulase with optimization and application'
 #buscareje(df1[0],tira.split())
 
 titulodict = 'N'
