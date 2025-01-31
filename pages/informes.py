5464213bae7bb6c9dd4f5d53d8be37f05733699d
miraki <derquicia@mariogoldsteinsa.com.ar>
2025-01-31 11:06:11 -0300

    Update informes.py

--- pages/informes.py
+++ pages/informes.py
@@ -13,6 +13,8 @@ from pyecharts import options as opts
 from pyecharts.charts import Line
 #import streamlit-wordcloud as wordcloud
 from st_wordcloud import st_wordcloud
+import networkx as nx
+from pyvis.network import Network
 
 st.set_page_config(initial_sidebar_state="collapsed",
                   layout="wide",menu_items=None,page_title="Miraki🖼")
@@ -142,5 +144,53 @@ with tab2:
 """  
 with tab3:
     st.header("Relaciones")
+    conn = st.connection("postgresql", type="sql")
+    df = conn.query('select titulo_es,detalle_es from novedades where nro_reporte is not null limit 100 ;', ttl="0")
+    G = nx.from_pandas_edgelist(df, 'drug_1_name', 'drug_2_name', 'weight')
+
+    drug_net = Network(
+                       height='400px',
+                       width='100%',
+                       bgcolor='#222222',
+                       font_color='white'
+                      )
+
+    # Take Networkx graph and translate it to a PyVis graph format
+    drug_net.from_nx(G)
+
+    # Generate network with specific layout settings
+    drug_net.repulsion(
+                        node_distance=420,
+                        central_gravity=0.33,
+                        spring_length=110,
+                        spring_strength=0.10,
+                        damping=0.95
+                       )
+
+    # Save and read graph as HTML file (on Streamlit Sharing)
+    try:
+        path = '/tmp'
+        drug_net.save_graph(f'{path}/pyvis_graph.html')
+        HtmlFile = open(f'{path}/pyvis_graph.html', 'r', encoding='utf-8')
+
+    # Save and read graph as HTML file (locally)
+    except:
+        path = '/html_files'
+        drug_net.save_graph(f'{path}/pyvis_graph.html')
+        HtmlFile = open(f'{path}/pyvis_graph.html', 'r', encoding='utf-8')
+
+    # Load HTML file in HTML component for display on Streamlit page
+    components.html(HtmlFile.read(), height=435)
+
+# Footer
+st.markdown(
+    """
+    <br>
+    <h6><a href="https://github.com/kennethleungty/Pyvis-Network-Graph-Streamlit" target="_blank">GitHub Repo</a></h6>
+    <h6><a href="https://kennethleungty.medium.com" target="_blank">Medium article</a></h6>
+    <h6>Disclaimer: This app is NOT intended to provide any form of medical advice or recommendations. Please consult your doctor or pharmacist for professional advice relating to any drug therapy.</h6>
+    """, unsafe_allow_html=True
+    )
+
 
 
