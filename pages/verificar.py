6c6051110e46d3717095be8a4c7026ff4ac086a2
miraki <derquicia@mariogoldsteinsa.com.ar>
2025-01-31 12:05:36 -0300

    Update verificar.py

--- pages/verificar.py
+++ pages/verificar.py
@@ -11,6 +11,7 @@ import json
 import pandas as pd
 from sqlalchemy import text
 from urllib.request import urljoin
+from streamlit_option_menu import option_menu
 
 st.set_page_config(initial_sidebar_state="collapsed",
                   layout="wide",menu_items=None,page_title="Miraki")
