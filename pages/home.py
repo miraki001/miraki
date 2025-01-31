3826eef9e84d8667baa8edcc015494fe4a684ad8
miraki <derquicia@mariogoldsteinsa.com.ar>
2025-01-31 15:40:10 -0300

    Update home.py

--- pages/home.py
+++ pages/home.py
@@ -9,7 +9,8 @@ from nltk.stem.porter import *
 from gensim.models import word2vec 
 from sklearn.manifold import TSNE 
 from nltk.tokenize import word_tokenize
-import streamlit.components.v1 as components
+from streamlit_elements import elements, mui, html, sync
+
 stemmer = PorterStemmer()  
 
 st.set_page_config(layout="wide",page_title="Miraki")
@@ -79,27 +80,40 @@ st.write(
 )
 
 
-imageCarouselComponent = components.declare_component("image-carousel-component", path="frontend/public")
-
-imageUrls = [
-        "https://images.unsplash.com/photo-1522093007474-d86e9bf7ba6f?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=764&q=80",
-        "https://images.unsplash.com/photo-1610016302534-6f67f1c968d8?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=1075&q=80",
-        "https://images.unsplash.com/photo-1516550893923-42d28e5677af?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=872&q=80",
-        "https://images.unsplash.com/photo-1541343672885-9be56236302a?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=687&q=80",
-        "https://images.unsplash.com/photo-1512470876302-972faa2aa9a4?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=870&q=80",
-        "https://images.unsplash.com/photo-1528728329032-2972f65dfb3f?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=870&q=80",
-        "https://images.unsplash.com/photo-1557744813-846c28d0d0db?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=1118&q=80",
-        "https://images.unsplash.com/photo-1513635269975-59663e0ac1ad?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=870&q=80",
-        "https://images.unsplash.com/photo-1595867818082-083862f3d630?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=870&q=80",
-        "https://images.unsplash.com/photo-1622214366189-72b19cc61597?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=687&q=80",
-        "https://images.unsplash.com/photo-1558180077-09f158c76707?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=764&q=80",
-        "https://images.unsplash.com/photo-1520106212299-d99c443e4568?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=687&q=80",
-        "https://images.unsplash.com/photo-1534430480872-3498386e7856?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=870&q=80",
-        "https://images.unsplash.com/photo-1571317084911-8899d61cc464?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=870&q=80",
-        "https://images.unsplash.com/photo-1624704765325-fd4868c9702e?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=764&q=80",
+IMAGES = [
+    "https://unsplash.com/photos/GJ8ZQV7eGmU/download?force=true&w=1920",
+    "https://unsplash.com/photos/eHlVZcSrjfg/download?force=true&w=1920",
+    "https://unsplash.com/photos/zVhYcSjd7-Q/download?force=true&w=1920",
+    "https://unsplash.com/photos/S5uIITJDq8Y/download?ixid=MnwxMjA3fDB8MXxhbGx8fHx8fHx8fHwxNjUyOTAzMzAz&force=true&w=1920",
+    "https://unsplash.com/photos/E4bmf8BtIBE/download?ixid=MnwxMjA3fDB8MXxhbGx8fHx8fHx8fHwxNjUyOTEzMzAw&force=true&w=1920",
 ]
-selectedImageUrl = imageCarouselComponent(imageUrls=imageUrls, height=200)
 
-if selectedImageUrl is not None:
-    st.image(selectedImageUrl)
 
+def slideshow_swipeable(images):
+    # Generate a session state key based on images.
+    key = f"slideshow_swipeable_{str(images).encode().hex()}"
+
+    # Initialize the default slideshow index.
+    if key not in st.session_state:
+        st.session_state[key] = 0
+        # Get the current slideshow index.
+    index = st.session_state[key]
+
+    # Create a new elements frame.
+    with elements(f"frame_{key}"):
+
+        # Use mui.Stack to vertically display the slideshow and the pagination centered.
+        # https://mui.com/material-ui/react-stack/#usage
+        with mui.Stack(spacing=2, alignItems="center"):
+
+            with mui.SwipeableViews(index=index, resistance=True, onChangeIndex=sync(key)):
+                for image in images:
+                    html.img(src=image, css={"width": "100%"})
+            def handle_change(event, value):
+                # Pagination starts at 1, but our index starts at 0, explaining the '-1'.
+                st.session_state[key] = value-1
+
+            mui.Pagination(page=index+1, count=len(images), color="primary", onChange=handle_change)
+
+
+slideshow_swipeable(IMAGES)                    
