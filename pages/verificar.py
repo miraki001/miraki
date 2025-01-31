c9c25685988e30911f031325918da416b8b645b8
miraki <derquicia@mariogoldsteinsa.com.ar>
2025-01-31 13:32:48 -0300

    Update verificar.py

--- pages/verificar.py
+++ pages/verificar.py
@@ -16,6 +16,8 @@ import numpy as np
 
 def buscareje(df,tira, case: bool = False):
   for texto in df['palabraclave_es']:
+       if search(texto,tira): 
+         st.write('si')
       st.write(texto)
 
 st.set_page_config(initial_sidebar_state="collapsed",
