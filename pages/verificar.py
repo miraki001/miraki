981ab786b62e5276f8c2dc829208ddca4803089c
miraki <derquicia@mariogoldsteinsa.com.ar>
2025-01-31 15:10:47 -0300

    Update verificar.py

--- pages/verificar.py
+++ pages/verificar.py
@@ -47,7 +47,8 @@ def buscarpalabras(df,tira):
        if index > 0:
          result = df[df['palabra'] == texto]
          #st.write(result)
-         vpeso = result.to_numeric(columns=['peso'], header=False, index=False)[0]
+         peso = result.to_string(columns=['peso'], header=False, index=False)[0]
+         vpeso = vpeso + int(peso)
          st.session_state['vejenuri'] = result.to_string(columns=['eje_nuri'], header=False, index=False)[0]
   return vpeso
 
