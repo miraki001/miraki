247adde919626f85f3ee8b19e652849de2957cb4
miraki <derquicia@mariogoldsteinsa.com.ar>
2025-02-01 10:41:31 -0300

    Update verificar.py

--- pages/verificar.py
+++ pages/verificar.py
@@ -149,7 +149,7 @@ if p > 0:
     sep = xtitulo[:p-2]
     titulodict = 'S'
     sep =sep.replace('"','')
-    #st.write(sep)
+    st.write(sep)
     resto = xtitulo[p:100]
     #st.write(resto)
     p = resto.find(":")
