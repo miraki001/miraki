61442bc4f17a3b3a1df98fd86d533f7c3e4c4e8f
miraki <derquicia@mariogoldsteinsa.com.ar>
2025-02-03 16:25:52 -0300

    Update verificar.py

--- pages/verificar.py
+++ pages/verificar.py
@@ -21,6 +21,7 @@ from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.chrome.service import Service
 from selenium.webdriver.support import expected_conditions as EC
 from selenium.webdriver.common.by import By
+from webdriver_manager.chrome import ChromeDriverManager
 
 
 def get_driver():
