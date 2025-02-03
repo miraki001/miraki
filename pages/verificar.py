b515bfd8cd67ca9b3b9743ed5431692cceae8c59
miraki <derquicia@mariogoldsteinsa.com.ar>
2025-02-03 17:07:58 -0300

    Update verificar.py

--- pages/verificar.py
+++ pages/verificar.py
@@ -327,7 +327,7 @@ if tipobusq != 'json' and tipobusq != 'rss' :
         options.add_argument('--log-level=3')
         driver = get_driver()
         driver.implicitly_wait(10)
-        driver.get('https://pubs.acs.org/action/doSearch?field1=AllField&target=default&targetTab=std&text1=grape&startPage=&sortBy=Earliest')
+        driver.get('https://cals.cornell.edu/viticulture-enology/news-events')
         #driver.implicitly_wait(40)
         wait = WebDriverWait(driver, 170)
         #wait.until(EC.url_contains("code="))
