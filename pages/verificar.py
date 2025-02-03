48de7bf363de5ec967fc741cf4574be5ae3cefc9
miraki <derquicia@mariogoldsteinsa.com.ar>
2025-02-03 14:20:49 -0300

    Update verificar.py

--- pages/verificar.py
+++ pages/verificar.py
@@ -207,7 +207,8 @@ st.write(newv)
 
 if tipobusq == 'rss':
   resp = requests.get(vurl)
-  soup = BeautifulSoup(resp.text, 'html.parser')
+  #soup = BeautifulSoup(resp.text, 'html.parser')
+  soup = BeautifulSoup(resp.text, 'lxml')
   pp = soup.find_all(separador)
   st.write(pp)
   for entry in soup.find_all(separador):
