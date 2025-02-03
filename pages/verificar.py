66dc5de578667c9513d3ffa9f652f9b512881a81
miraki <derquicia@mariogoldsteinsa.com.ar>
2025-02-03 12:02:48 -0300

    Update verificar.py

--- pages/verificar.py
+++ pages/verificar.py
@@ -218,13 +218,13 @@ if tipobusq== 'json':
     soup1 = BeautifulSoup(r.content, 'html.parser')
     #pp = soup1.find_all('script')[14].text.strip()[48:-1]
     #pp = soup1.find_all('script')[14].text.strip()
-    data = page_soup.findAll('script', {'type':'application/ld+json'})[2]
+    pp = page_soup.findAll('script', {'type':'application/ld+json'})[2]
     st.write(data)
     data = [
       json.loads(x.string) for x in soup1.find_all("script", type="application/ld+json")
     ]
   
-    pp = soup1.find_all(separador,"type:'application/ld+json'")
+    #pp = soup1.find_all(separador,"type:'application/ld+json'")
     st.write(pp)
     #st.write('fffffff')
     #data = page_soup.select("[type='application/json']")[vpos]
