8c99d257719d22564141c55789477b104c8326e7
miraki <derquicia@mariogoldsteinsa.com.ar>
2025-02-04 15:01:23 -0300

    Update verificar.py

--- pages/verificar.py
+++ pages/verificar.py
@@ -254,7 +254,7 @@ if tipobusq == 'rss':
   #st.write(pp)
   for entry in soup.find_all(separador):
     tit = entry.find(xtitulo).text
-    ll = entry(find(re.compile("^link") )
+    ll = entry.find(re.compile("^link") )
     
     st.write(ll)
     st.write(xxlink)
