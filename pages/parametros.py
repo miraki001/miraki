a216459ca53c3cb035ef41918c69f0483875be16
miraki <derquicia@mariogoldsteinsa.com.ar>
2025-02-06 10:29:30 -0300

    Update parametros.py

--- pages/parametros.py
+++ pages/parametros.py
@@ -70,7 +70,7 @@ unsafe_allow_html=True)
 
 st.write(
     """
-        **Palabras claves : **Son usadas en las busqueda individuales de las paginas, en el caso que la novedades no
+        ** Palabras claves : ** Son usadas en las busqueda individuales de las paginas, en el caso que la novedades no
         estemos seguros que cumplen todas las condiciones de los temas que queremos ubicar.
         Por ejemplo en las busquedas de noticias en los diarios.
         
