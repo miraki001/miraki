c1d8f9707d96911da16ba16c2d88bfdbbc06ec18
miraki <derquicia@mariogoldsteinsa.com.ar>
2025-02-06 12:48:03 -0300

    Update informes.py

--- pages/informes.py
+++ pages/informes.py
@@ -22,8 +22,9 @@ st.set_page_config(initial_sidebar_state="collapsed",
 
 
 st.logo(
-    "https://firebasestorage.googleapis.com/v0/b/miraki-7ca50.appspot.com/o/android-chrome-192x192.png?alt=media&token=e8d5f6ee-706a-4399-813d-fd82f8b35ec7",
-    icon_image="https://firebasestorage.googleapis.com/v0/b/miraki-7ca50.appspot.com/o/android-chrome-192x192.png?alt=media&token=e8d5f6ee-706a-4399-813d-fd82f8b35ec7",
+    "https://firebasestorage.googleapis.com/v0/b/miraki-7ca50.appspot.com/o/MARCA%20ARRIBA%20PNG.png?alt=media&token=46705f1e-7f86-4d2b-b2ab-a7188a30b379",
+    icon_image="https://firebasestorage.googleapis.com/v0/b/miraki-7ca50.appspot.com/o/MARCA%20ARRIBA%20PNG.png?alt=media&token=46705f1e-7f86-4d2b-b2ab-a7188a30b379",
+    size='large',
 )
 
 #https://github.com/drogbadvc/st-wordcloud
@@ -41,7 +42,7 @@ st.markdown(
                .block-container {
                     padding-top: 0rem;
                     padding-bottom: 0rem;
-                    padding-left: 4rem;
+                    padding-left: 5rem;
                     padding-right: 2rem;
                 }
         </style>
