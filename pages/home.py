661fcc7f57bb06187f3dd06fe559d50f093238be
miraki <derquicia@mariogoldsteinsa.com.ar>
2025-02-06 11:39:24 -0300

    Update home.py

--- pages/home.py
+++ pages/home.py
@@ -15,7 +15,7 @@ stemmer = PorterStemmer()
 
 
 fontuse = """
-@import url('https://fonts.googleapis.com/css2?family=Roboto:wght@400;700&display=swap');
+@import url('https://fonts.googleapis.com/css2?family=Calibri:wght@400;700&display=swap');
 
 body {
     font-family: 'Calibri', sans-serif;
