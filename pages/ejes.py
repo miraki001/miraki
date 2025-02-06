cc14c1025a26c9f0f67ec72bfc055687e7f00328
miraki <derquicia@mariogoldsteinsa.com.ar>
2025-02-06 12:49:37 -0300

    Update ejes.py

--- pages/ejes.py
+++ pages/ejes.py
@@ -22,7 +22,7 @@ st.markdown(
                .block-container {
                     padding-top: 0rem;
                     padding-bottom: 0rem;
-                    padding-left: 2rem;
+                    padding-left: 5rem;
                     padding-right: 2rem;
                 }
         </style>
@@ -30,9 +30,14 @@ st.markdown(
     unsafe_allow_html=True,
 )
 
+st.logo(
+    "https://firebasestorage.googleapis.com/v0/b/miraki-7ca50.appspot.com/o/MARCA%20ARRIBA%20PNG.png?alt=media&token=46705f1e-7f86-4d2b-b2ab-a7188a30b379",
+    icon_image="https://firebasestorage.googleapis.com/v0/b/miraki-7ca50.appspot.com/o/MARCA%20ARRIBA%20PNG.png?alt=media&token=46705f1e-7f86-4d2b-b2ab-a7188a30b379",
+  size='large',
+)
 
 
-st.subheader("Miraki - Ejes")
+st.subheader("Ejes")
 
 def borrar():
     tnuri = st.session_state['vnuri']
