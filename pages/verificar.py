66e527164b91c1d1c7fa3b39720688b53842e20b
miraki <derquicia@mariogoldsteinsa.com.ar>
2025-02-08 20:18:12 -0300

    Update verificar.py

--- pages/verificar.py
+++ pages/verificar.py
@@ -285,7 +285,7 @@ if tipobusq == 'rss':
     #img =  vimg['url']  
     det = re.sub(CLEANR, '', det)
     
-    det =  det.encode('latin-1')
+    #det =  det.encode('latin-1')
     st.write(tit)
     st.write(det)
     st.write(link)
