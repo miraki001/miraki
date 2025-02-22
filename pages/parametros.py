f779b3c9e8a3f5b351a81854e93c03c824d746b4
miraki <derquicia@mariogoldsteinsa.com.ar>
2025-02-22 12:22:25 -0300

    Update parametros.py

--- pages/parametros.py
+++ pages/parametros.py
@@ -37,7 +37,7 @@ st.logo(
 st.subheader("    Parametros")
 
 selected3 = option_menu(None, ["Home", "Palabras Claves","Excluidas","Sectores","Ejes","Palabras por Sector","Proyectos" ], 
-      icons=['house', 'alphabet' ,'x-circle','diagram-3','list-check','alphabet-uppercase','building-fill' ] , menu_icon="cast",orientation="horizontal", default_index = -2,
+      icons=['house', 'alphabet' ,'x-circle','diagram-3','list-check','alphabet-uppercase','building-fill' ] , menu_icon="cast",orientation="horizontal", default_index = 0,
                 
       styles={
         "container": {"padding": "0!important", "background-color": "#604283"},
