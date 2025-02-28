6bdeb902ef7e33f29b17c9dc610725b2fb80c547
miraki <derquicia@mariogoldsteinsa.com.ar>
2025-02-28 17:03:05 -0300

    Update login.py

--- pages/login.py
+++ pages/login.py
@@ -62,7 +62,7 @@ with col[0]:
     vquery = "select count(0) cnt from usuarios where usuario = :usuario and clave = :clave  ;"
     df2 = conn.query(vquery, ttl="0",params={"usuario": vusuario, "clave" :vclave }),
     nuri = df2[0].to_string(columns=['cnt'], header=False, index=False)
-    #st.write(nuri)
+    st.write(nuri)
     if nuri == 0:
       st.write('Usuario no existe o clave incorrecta')
     if nuri == '':
@@ -78,7 +78,7 @@ with col[0]:
         df1 = conn.query('select nuri,proyecto from proyectos ;', ttl="0"),
         df = df1[0]
         #vpro = st.selectbox(' Ingrese en el Proyecto que va trabajar ', df.proyecto )
-        st.session_state['vpro'] = vpro
+        st.session_state['vpro'] = proy_nuri
         st.session_state['usuario'] = vusuario
 
 
