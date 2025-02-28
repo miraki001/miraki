95f64ae1fa72e910a1f46749825634e2bc261a37
miraki <derquicia@mariogoldsteinsa.com.ar>
2025-02-28 15:07:06 -0300

    Update login.py

--- pages/login.py
+++ pages/login.py
@@ -74,10 +74,10 @@ with col[0]:
         conn = st.connection("postgresql", type="sql")
         df1 = conn.query('select nuri,proyecto from proyectos ;', ttl="0"),
         df = df1[0]
-        vpro = st.selectbox(' Ingrese en el Proyecto que va trabajar ', df.proyecto )
-        st.session_state['vpro'] = vpro
-        st.session_state['usuario'] = vusuario
-        st.write(vpro)
+        #vpro = st.selectbox(' Ingrese en el Proyecto que va trabajar ', df.proyecto )
+        #st.session_state['vpro'] = vpro
+        #st.session_state['usuario'] = vusuario
+        #st.write(vpro)
 
         #st.button('Ingresar', on_click=ingresar(admin))
 
