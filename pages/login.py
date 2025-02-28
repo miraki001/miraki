3eef528efa1a2de07f58a19b80a30ea6ebcaa085
miraki <derquicia@mariogoldsteinsa.com.ar>
2025-02-28 14:15:13 -0300

    Update login.py

--- pages/login.py
+++ pages/login.py
@@ -53,6 +53,30 @@ col = st.columns((6.5, 4.5, 2), gap='medium')
 with col[0]:
   vusuario = st.text_input("Ingreseo su nombre de usuario")
   vclave = st.text_input("ingrese su Contraseña")
+  if vusuario !='' and vclave != '':
+    conn = st.connection("postgresql", type="sql")
+    vquery = "select count(0) cnt from usuarios where usuario = :usuario and clave = :clave  ;"
+    df2 = conn.query(vquery, ttl="0",params={"usuario": vusuario, "clave" :vclave }),
+    nuri = df2[0].to_string(columns=['cnt'], header=False, index=False)
+    st.write(nuri)
+    if nuri == 0:
+      st.write('Usuario no existe o clave incorrecta')
+    if nuri != 0:
+
+        vquery = "select administrador, proyecto_nuri  from usuarios where usuario = :usuario and clave = :clave  ;"
+        df2 = conn.query(vquery, ttl="0",params={"usuario": vusuario, "clave" :vclave }),
+        admin = df2[0].to_string(columns=['administrador'], header=False, index=False)
+        proy_nuri = df2[0].to_string(columns=['proyecto_nuri'], header=False, index=False)
+        st.write(admin)
+      
+        conn = st.connection("postgresql", type="sql")
+        df1 = conn.query('select nuri,proyecto from proyectos ;', ttl="0"),
+        df = df1[0]
+        vpro = st.selectbox(' Ingrese en el Proyecto que va trabajar ', df.proyecto )
+        st.session_state['vpro'] = vpro
+        st.session_state['usuario'] = vusuario
+        st.write(vpro)
+    
 
 
   with st.form(key="my-form"):
