214843d2966251e9abe85aabc6775ce0cf39afd5
miraki <derquicia@mariogoldsteinsa.com.ar>
2025-02-28 17:07:14 -0300

    Update login.py

--- pages/login.py
+++ pages/login.py
@@ -62,14 +62,12 @@ with col[0]:
     vquery = "select count(0) cnt from usuarios where usuario = :usuario and clave = :clave  ;"
     df2 = conn.query(vquery, ttl="0",params={"usuario": vusuario, "clave" :vclave }),
     nuri = df2[0].to_string(columns=['cnt'], header=False, index=False)
-    st.write(nuri)
+    #st.write(nuri)
     
     if nuri == '0':
       st.write('Usuario no existe o clave incorrecta')
-    if nuri == '':
-      st.write('Usuario no existe o clave incorrecta')
       
-    if nuri != 0:
+    if nuri != '0':
 
         vquery = "select administrador, proyecto_nuri  from usuarios where usuario = :usuario and clave = :clave  ;"
         df2 = conn.query(vquery, ttl="0",params={"usuario": vusuario, "clave" :vclave }),
@@ -84,9 +82,7 @@ with col[0]:
 
 
     
-    if nuri != 0:
+    if nuri != '0':
         ingresar(admin)
-    if nuri =='':
-        st.write('Usuario no existe o clave incorrecta')
-    if nuri == 0:
+    if nuri == '0':
         st.write('Usuario no existe o clave incorrecta')
