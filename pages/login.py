451e48e4f1549caa66d504ed8d094f6700f75647
miraki <derquicia@mariogoldsteinsa.com.ar>
2025-03-04 11:11:10 -0300

    Update login.py

--- pages/login.py
+++ pages/login.py
@@ -78,6 +78,7 @@ with col[0]:
         df = df1[0]
         #vpro = st.selectbox(' Ingrese en el Proyecto que va trabajar ', df.proyecto )
         st.session_state['vpro'] = proy_nuri
+        st.session_state['vproyecto'] = proy_nuri
         st.session_state['usuario'] = vusuario
         st.session_state['vadmin'] = admin
 
