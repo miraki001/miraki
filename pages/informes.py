25e84d8298cf85d630316ecd356671f4429c3ae7
miraki <derquicia@mariogoldsteinsa.com.ar>
2025-03-17 16:55:27 -0300

    Update informes.py

--- pages/informes.py
+++ pages/informes.py
@@ -58,10 +58,10 @@ st.subheader("Informes")
 selected2 = option_menu(None, ["Miraki", 'Novedades','Fuentes', 'Informes','Parametros','Github' ], 
         icons=['house', 'newspaper' , 'filetype-html','globe-americas','gear','github'] , menu_icon="cast",orientation="horizontal", default_index=0,
         styles={
-        "container": {"padding": "0!important", "background-color": "#604283"},
+        "container": {"padding": "0!important", "background-color": "#898989"},
         "icon": {"color": "orange", "font-size": "14px"}, 
         "nav-link": {"color": "white",  "font-size": "14px", "text-align": "left", "margin":"0px", "--hover-color": "#B3D3F0"},
-        "nav-link-selected": {"background-color": "#604283"}
+        "nav-link-selected": {"background-color": "#898989"}
         }
   )
 
