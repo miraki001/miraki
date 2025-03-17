20a78b3dee8fa65a2696b10a342504ca877e781c
miraki <derquicia@mariogoldsteinsa.com.ar>
2025-03-17 16:56:25 -0300

    Update novedadessola.py

--- pages/novedadessola.py
+++ pages/novedadessola.py
@@ -87,10 +87,10 @@ selected241 = option_menu(None, ["Novedades", 'Ingresar','Editar','Borrar','Sele
       icons=['newspaper', 'plus' ,'pencil-square','eraser','chek','patch-chek','building-fill' ] , menu_icon="cast",orientation="horizontal", default_index=0,
                 
       styles={
-        "container": {"padding": "0!important", "background-color": "#604283"},
+        "container": {"padding": "0!important", "background-color": "#898989"},
         "icon": {"color": "orange", "font-size": "14px"}, 
         "nav-link": {"color": "white", "font-size": "14px", "text-align": "left", "margin":"0px", "--hover-color": "#B3D3F0"},
-        "nav-link-selected": {"background-color": "#604283"}
+        "nav-link-selected": {"background-color": "#898989"}
       }
 )
 
