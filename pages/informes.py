a91755a660d369ef726570b0d5f57ae5a08781db
miraki <derquicia@mariogoldsteinsa.com.ar>
2025-03-18 16:23:25 -0300

    Update informes.py

--- pages/informes.py
+++ pages/informes.py
@@ -16,7 +16,7 @@ from st_wordcloud import st_wordcloud
 import networkx as nx
 from pyvis.network import Network
 import streamlit.components.v1 as components
-from pyBibX import SimilarityAnalyzer
+
 
 st.set_page_config(initial_sidebar_state="collapsed",
                   layout="wide",menu_items=None,page_title="Miraki🖼")
@@ -183,19 +183,47 @@ with tab3:
     components.html(HtmlFile.read(), height=635)
 
 with tab4:
-    st.header("Otros")
+    st.header("Relaciones")
     conn = st.connection("postgresql", type="sql")
-    df = conn.query('select palabra as text,cnt as value from tag_words() ;', ttl="0")
-    analyzer = SimilarityAnalyzer(df)
+    df = conn.query('select titulo || detalle detalle,categoria from nov limit 100 ;', ttl="0")
+    G = nx.from_pandas_edgelist(df,source='detalle', target='categoria' )
 
-    similarity_results = analyzer.analyze_similarity()
+    drug_net = Network(
+                       select_menu = true,
+                       height='800px',
+                       width='100%',
+                       bgcolor='#222222',
+                       font_color='white'
+                      )
 
-    st.write(similarity_results)
-  
-    words = df.to_dict('records')
-    #st.write(words)
-    #words = [{"text": "Python", "value": 500, "topic": "lol"}, {"text": "Streamlit", "value": 80},{"text": "Streamlit", "value": 80},{"text": "Streamlit", "value": 80},{"text": "Streamlit", "value": 80},{"text": "Streamlit", "value": 80},{"text": "Streamlit", "value": 80}]
+    # Take Networkx graph and translate it to a PyVis graph format
+    drug_net.from_nx(G)
 
-    st_wordcloud(words, width=800, height=600)
+    # Generate network with specific layout settings
+    drug_net.repulsion(
+                        node_distance=420,
+                        central_gravity=0.33,
+                        spring_length=110,
+                        spring_strength=0.10,
+                        damping=0.95
+                       )
+
+    # Save and read graph as HTML file (on Streamlit Sharing)
+
+    try:
+        path = '/tmp'
+        drug_net.save_graph(f'{path}/pyvis_graph.html')
+        HtmlFile = open(f'{path}/pyvis_graph.html', 'r', encoding='utf-8')
+
+    # Save and read graph as HTML file (locally)
+    except:
+        path = '/html_files'
+        drug_net.save_graph(f'{path}/pyvis_graph.html')
+        HtmlFile = open(f'{path}/pyvis_graph.html', 'r', encoding='utf-8')
+
+    # Load HTML file in HTML component for display on Streamlit page
+    components.html(HtmlFile.read(), height=635)
+
+
 
 
