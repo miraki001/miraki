22cccfd8f1ce51fbb7f931c7a318839043c201f2
miraki <derquicia@mariogoldsteinsa.com.ar>
2025-07-08 13:11:14 -0300

    Update analizar.py

--- pages/analizar.py
+++ pages/analizar.py
@@ -5,9 +5,9 @@ from sqlalchemy import text
 from streamlit_option_menu import option_menu
 
 conn = st.connection("postgresql", type="sql")
-activa = "S"
+activa = 'S'
 qq = 'select nuri,fuente as url,activa,fecha_act,descrip as fuente,pais,fuente_org,urllink,tipo_busq,posjson,tipo_img,postit,posdet from fuentes_py where proyecto_nuri = 1 ;'
 df1 = conn.query(qq, ttl="0"),
 df = df1[0]
-df = df[df['activa'].isin("S")]
+df = df[df['activa'].isin(activa)]
 st.write(df.len)
