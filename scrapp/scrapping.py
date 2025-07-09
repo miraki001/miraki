24f50ba8fd81cb62d75d2f4ee3e79a2a03f0b069
miraki <derquicia@mariogoldsteinsa.com.ar>
2025-07-09 13:08:14 -0300

    Update scrapping.py

--- scrapp/scrapping.py
+++ scrapp/scrapping.py
@@ -432,10 +432,9 @@ def scrapping():
         peso = 0
         eje_nuri = buscareje(df1[0],title + ' ' + det)
         peso = buscarpalabras(df2[0],title + ' ' + det)
-        st.write("aca antes     ")
         if eje_nuri == None:
           eje_nuri = 6
-          st.write("aca")
+
       
         ap = pd.DataFrame([{'tit': title, 'det': det, 'link': href,'img': img,'eje': eje_nuri,'peso': peso}])
         dres = pd.concat([dres,ap])            
