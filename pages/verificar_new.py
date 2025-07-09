ec4e3a2f3edd65a696bc971a20b21bef16b6d6ad
miraki <derquicia@mariogoldsteinsa.com.ar>
2025-07-09 08:25:17 -0300

    Update verificar_new.py

--- pages/verificar_new.py
+++ pages/verificar_new.py
@@ -1,1 +1,12 @@
+import streamlit as st
+import os
+import re
+import sys
+import psycopg2
+import json
+import pandas as pd
+from sqlalchemy import text
+from streamlit_option_menu import option_menu
+import numpy as np
+import time
 
