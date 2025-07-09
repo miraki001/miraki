40431bef4bdc14f095d0e272ee3a062732f55c64
miraki <derquicia@mariogoldsteinsa.com.ar>
2025-07-09 11:16:51 -0300

    Update analizar.py

--- pages/analizar.py
+++ pages/analizar.py
@@ -14,7 +14,7 @@ def buscar_not(vtitu,vfuente,vproyecto):
     buscar = buscar + " and fuente_nuri = :fuente"
     buscar = buscar + " and proyecto_nuri = :proyecto ;"
     df2 = conn.query(buscar, ttl="0",params={"titu": vtitu,"fuente": vfuente,"proyecto": vproyecto})
-    st.write(df2)
+    st.write(df2[0])
     vcnt = df2[0].to_string(columns=['cnt'], header=False, index=False)
     return vcnt
 
