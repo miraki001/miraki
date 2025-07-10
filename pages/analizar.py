e869b05e28057d56813571e86e26fdc8f02adde0
miraki <derquicia@mariogoldsteinsa.com.ar>
2025-07-10 11:58:52 -0300

    Update analizar.py

--- pages/analizar.py
+++ pages/analizar.py
@@ -21,11 +21,11 @@ def buscar_not(vtitu,vfuente,vproyecto):
     #st.write(cnt)
     return cnt
 
-def ingresar(vtitu,vfuente,vproyecto,vfuente_nuri,vdet,vlink,vimg,vpeso):
+def ingresar(vtitu,vfuente,vproyecto,vfuente_nuri,vdet,vlink,veje,vimg,vpeso):
       with conn.session as session:
         actualiza = "INSERT INTO novedades( nuri, fuente, titulo,detalle,tipo,fecha,selec,origen,link,tema,proyecto_nuri,select_web,selec_alerta,imagen,fuente_nuri,eje_nuri,orden,leido,puntaje) "
         actualiza = actualiza + "VALUES (nextval('novedades_seq'), :fuente, :titulo,:detalle,:tipo,current_date,:selec,:origen,:link,:tema,:proyecto_nuri,:web,:alerta,:imagen,:fuente_nuri,:eje_nuri,:nro,:leido,:puntaje);"
-        session.execute(text(actualiza), {"fuente": vfuente,"titulo": vtitu,"detalle": vdet,"tipo": 'N',"selec": 'N',"origen": 'A',"link": vlink,"tema": '',"proyecto_nuri": vproyecto,"web": 'N',"alerta": 'N',"imagen": vimg,"fuente_nuri": vfuente_nuri,"nro": 5,"leido": 'N',"puntaje": vpeso})
+        session.execute(text(actualiza), {"fuente": vfuente,"titulo": vtitu,"detalle": vdet,"tipo": 'N',"selec": 'N',"origen": 'A',"link": vlink,"tema": '',"proyecto_nuri": vproyecto,"web": 'N',"alerta": 'N',"imagen": vimg,"fuente_nuri": vfuente_nuri,"eje_nuri": veje,"nro": 5,"leido": 'N',"puntaje": vpeso})
         session.commit()
 activa = "S"
 qq = 'select * from fuentes_py where proyecto_nuri = 1 and nuri = 6073 ;'
@@ -86,10 +86,10 @@ for index in range(len(df)) :
       encontrada = buscar_not(tit,int(fnuri),int(vpro))
       st.write("encontrada :" + str(encontrada))
       if buscar_pers == 'S' and peso > 3:
-        ingresar(tit,fuente,int(vpro),int(fnuri),det,link,img,peso)
+        ingresar(tit,fuente,int(vpro),int(fnuri),det,link,eje,img,peso)
         vcnt1 = vcnt1 + 1
       if buscar_pers == 'N':
-        ingresar(tit,fuente,int(vpro),int(fnuri),det,link,img,peso)
+        ingresar(tit,fuente,int(vpro),int(fnuri),det,link,eje,img,peso)
         vcnt1 = vcnt1 + 1
 
 
