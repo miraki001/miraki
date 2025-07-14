7107f55e350ad53b793827c7331f36c9cc023d32
miraki <derquicia@mariogoldsteinsa.com.ar>
2025-07-14 11:34:46 -0300

    Update fuentes.py

--- pages/fuentes.py
+++ pages/fuentes.py
@@ -49,10 +49,12 @@ def duplicar():
   conn = st.connection("postgresql", type="sql")
   tnuri = int(st.session_state['vnuri'])
   with conn.session as session:
-    actualiza = "INSERT INTO fuentes_py( nuri, fuente, fecha_act, activa, tipo, descrip, proyecto_nuri, limite, tema, busqueda_pers, pais, idioma, separador, xpath_titulo, xpath_detalle, xpath_autores, sep_autores, cnt_noticias, utfdecode, xpath_publica, act_url_fecha, urlencode, cnt_encontradas, baja_def, observa, cod_pais, xpath_link, xpath_image, atributo1, atributo2, tipo_busq, fuente_org, urllink, posjson,tipo_img.postit,posdet)"
-    actualiza = actualiza  + " select nextval('fuentes_py_seq')  , fuente, fecha_act, activa, tipo, descrip, proyecto_nuri, limite, tema, busqueda_pers, pais, idioma, separador, xpath_titulo, xpath_detalle, xpath_autores, sep_autores, cnt_noticias, utfdecode, xpath_publica, act_url_fecha, urlencode, cnt_encontradas, baja_def, observa, cod_pais, xpath_link, xpath_image, atributo1, atributo2, tipo_busq, fuente_org, urllink, posjson,tipo_img,postit,posdet  " 
+    actualiza = "INSERT INTO fuentes_py( nuri, fuente, fecha_act, activa, tipo, descrip, proyecto_nuri, limite, tema, busqueda_pers, pais, idioma, separador, xpath_titulo, xpath_detalle, xpath_autores, sep_autores, cnt_noticias, utfdecode, xpath_publica, act_url_fecha, urlencode, cnt_encontradas, baja_def, observa, cod_pais, xpath_link, xpath_image, atributo1, atributo2, tipo_busq, fuente_org, urllink, posjson,tipo_img,postit,posdet,poslink)"
+    actualiza = actualiza  + " select nextval('fuentes_py_seq')  , fuente, fecha_act, activa, tipo, descrip, proyecto_nuri, limite, tema, busqueda_pers, pais, idioma, separador, xpath_titulo, xpath_detalle, xpath_autores, sep_autores, cnt_noticias, utfdecode, xpath_publica, act_url_fecha, urlencode, cnt_encontradas, baja_def, observa, cod_pais, xpath_link, xpath_image, atributo1, atributo2, tipo_busq, fuente_org, urllink, posjson,tipo_img,postit,posdet,poslink  " 
     actualiza = actualiza + " from fuentes_py where nuri = :nuri ;"
     session.execute(text(actualiza), {"nuri": tnuri})
+
+    
     session.commit()  
     st.info("la fuente ha sido duplicad")
 
