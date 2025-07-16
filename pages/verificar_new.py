f798c1ec2f875c8677a8203d00eb715d9e04631b
miraki <derquicia@mariogoldsteinsa.com.ar>
2025-07-16 19:49:50 -0300

    Update verificar_new.py

--- pages/verificar_new.py
+++ pages/verificar_new.py
@@ -76,7 +76,8 @@ dres = scrapping.scrapping()
 #dres = dres.mask(dres.eq('None')).dropna()
 #dres = dres.mask(dres.astype(object).eq('None')).dropna()
 #dres = dres.dropna(how='any',axis=0)
-if dres.empty == True:
+df_empty = dres()
+if df_empty == True:
   st.write("nada que mostrar")
 else:
   st.write("Cantidad de Noticias : " + str(len(dres)))
