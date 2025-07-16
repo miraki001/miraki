2e327ee2bcba80c6ee86c5b91a5cda8ee388488b
miraki <derquicia@mariogoldsteinsa.com.ar>
2025-07-16 19:30:05 -0300

    Update scrapping.py

--- scrapp/scrapping.py
+++ scrapp/scrapping.py
@@ -484,7 +484,7 @@ def scrapping():
         
         #st.write("sele")
     else:
-        st.write("otro hasta aca ")
+        st.write("otro hasta aca 1")
 
         headers = {
            'User-Agent': 'Mozilla/5.0 (X11; Linux x86_64; rv:109.0) Gecko/20100101 Firefox/117.0'
