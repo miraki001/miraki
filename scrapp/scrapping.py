ef9a74442dbdb6181c8b631b7eb001a78ca859ee
miraki <derquicia@mariogoldsteinsa.com.ar>
2025-07-16 19:35:52 -0300

    Update scrapping.py

--- scrapp/scrapping.py
+++ scrapp/scrapping.py
@@ -490,7 +490,7 @@ def scrapping():
            'User-Agent': 'Mozilla/5.0 (X11; Linux x86_64; rv:109.0) Gecko/20100101 Firefox/117.0'
         }      
         try:
-           response = requests.get(url,headers=headers)
+           response = requests.get(url,headers=headers, timeout=2)
            st.write("no no")
            st.write(response)
         except:
